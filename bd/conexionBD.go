9747bef2628b81f4259225bdfca0b786e96a113a
flebangmail <fleban@gmail.com>
2023-01-26 16:14:52 -0300

    c

---
+++ bd/conexionBD.go
@@ -0,0 +1,35 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://usuario:usuario@cluster0.iuftzrk.mongodb.net/?retryWrites=true&w=majority")
+
+func ConectarBD() *mongo.Client {
+	client,err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+		return client
+	}
+	log.Println("Conexion exitosa a la BD")
+	return client
+}
+
+func ChequeoConexion() bool {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return false
+	}
+	return true
+}
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"log"
+	"github.com/flebangmail/CursoGo/bd"
+	"github.com/flebangmail/CursoGo/handlers"
+)
 
 func main() {
-
+	if !bd.ChequeoConexion() {
+		log.Fatal("Sin Conexion a la BD") 
+	}
+	handlers.Manejadores()
 }
